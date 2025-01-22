95d884612af37bab674d4ee8269fce4284bbe423
Designlanky <Design.lanky@gmail.com>
2025-01-22 19:52:08 +0100

    Update client.js

--- src/components/lib/client.js
+++ src/components/lib/client.js
@@ -1,7 +1,7 @@
 import SanityClient from "@sanity/client";
 
 export const client = SanityClient({
-  projectId: "t9i72iv0",
+  projectId: "02bwb137",
   dataset: "production",
   apiVersion: "2024-09-26",
   useCdn: true,
